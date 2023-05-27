757e9ae3f06bdb0080400830923bdda953150583
Giacomo Galavotti <g.galavotti@disroot.org>
2023-05-27 16:05:21 +0200

    Added Scaffold + NavHost + refactor LoginActivity.kt to AuthenticationActivity.kt + Updated dependencies

--- app/src/main/java/it/unical/gciaoo/vinteddu_android/AuthenticationActivity.kt
+++ app/src/main/java/it/unical/gciaoo/vinteddu_android/AuthenticationActivity.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -16,29 +17,47 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import it.unical.gciaoo.vinteddu_android.ui.theme.Typography
 
-@Preview(showBackground = true)
+
 @Composable
-fun Login() {
-    Column(modifier = Modifier.padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-    Text(
-        stringResource(R.string.login), style = Typography.headlineLarge,
-        modifier = Modifier
-            .align(Alignment.Start)
-            .padding(vertical = 50.dp, horizontal = 10.dp))
+fun Login(navHostController: NavHostController) {
+    Column(
+        modifier = Modifier.padding(all = 5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(0.5f))
+        Text(
+            text = stringResource(R.string.login), style = Typography.headlineLarge,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(horizontal = 10.dp)
+                .weight(0.3f)
+        )
         InputField(name = stringResource(R.string.login_username))
         InputField(name = stringResource(R.string.login_password))
         Button(content = {
-            Text(stringResource(R.string.login))
+                Text(stringResource(R.string.login))
+
         },
             modifier = Modifier.padding(vertical = 30.dp),
-        onClick = {}
+            onClick = {
+
+            }
         )
-        Text(stringResource(R.string.register), modifier = Modifier.clickable {})
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            stringResource(R.string.register_prompt),
+            modifier = Modifier.clickable(onClick = { navHostController.navigate("login/new") }).padding(vertical = 10.dp)
+        )
     }
+}
+
+@Composable
+fun Register() {
+    //TODO: implement Register Widget
 }
 
 @Composable
@@ -47,10 +66,10 @@ fun InputField(name: String) {
     Row(horizontalArrangement = Arrangement.Center) {
         TextField(
             value = field,
-            onValueChange = {field = it},
-            label = {Text(name)},
+            onValueChange = { field = it },
+            label = { Text(name) },
             singleLine = true,
-            modifier = Modifier.padding(vertical = 10.dp),
+            modifier = Modifier.padding(vertical = 10.dp)
         )
     }
 }
--- app/src/main/java/it/unical/gciaoo/vinteddu_android/MainActivity.kt
+++ app/src/main/java/it/unical/gciaoo/vinteddu_android/MainActivity.kt
@@ -3,13 +3,33 @@ package it.unical.gciaoo.vinteddu_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import it.unical.gciaoo.vinteddu_android.ui.theme.VintedduAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomePage()
                 }
             }
         }
@@ -30,17 +50,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun NavigationView(navHostController: NavHostController) {
+    NavHost(navController = navHostController, startDestination = "login") {
+        composable("login") {
+            Login(navHostController = navHostController)
+        }
+        composable("login/new") {
+            Register()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    VintedduAndroidTheme {
-        Greeting("Android")
+fun VintedduTopAppBar(navHostController: NavHostController) {
+    val currentBackStackEntry by navHostController.currentBackStackEntryAsState()
+    val showBackIcon by remember(currentBackStackEntry) { derivedStateOf { navHostController.previousBackStackEntry != null }}
+    TopAppBar(title = {Text(stringResource(R.string.title))},
+        navigationIcon = {
+            if (showBackIcon) {
+                IconButton(onClick = { navHostController.popBackStack() }) {
+                    Icon (
+                        Icons.Filled.KeyboardArrowLeft,
+                        contentDescription = stringResource(R.string.back)
+                            )
+                }
+            }
+            else {
+                IconButton(onClick = {}) {
+                    Icon(Icons.Rounded.Menu, contentDescription = stringResource(R.string.drawer_icon))
+                }
+            }
+        }
+    )
+}
+@Preview
+@Composable
+fun HomePage() {
+    val navHostController = rememberNavController()
+
+    Scaffold(topBar = { VintedduTopAppBar(navHostController = navHostController) }) {
+        Box(modifier = Modifier.fillMaxSize().padding(it), contentAlignment = Alignment.Center) {
+            NavigationView(navHostController = navHostController)
+        }
     }
 }
