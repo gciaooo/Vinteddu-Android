6e782718965f37c3d1c529e0f688a7a3506f3d39
Casss <alessandrocasella1107@gmail.com>
2023-06-12 20:08:34 +0200

    modifica retrofit client, aggiunto sessionmanager e httpclient

--- app/src/main/java/it/unical/gciaoo/vinteddu_android/RetrofitClient.kt
+++ app/src/main/java/it/unical/gciaoo/vinteddu_android/RetrofitClient.kt
@@ -1,15 +1,22 @@
 package it.unical.gciaoo.vinteddu_android
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitClient {
     companion object {
-        private const val BASE_URL = "http://:8080" // Mettere l'indirizzo ip del pc tra // e :
+        private const val BASE_URL = "http://192.168.1.54:8080" // Mettere l'indirizzo ip del pc tra // e :
 
-        fun create(): ApiService {
+        fun create(sessionManager: SessionManager): ApiService {
+            val interceptor = AuthInterceptor(sessionManager)
+            val httpClient = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(httpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
